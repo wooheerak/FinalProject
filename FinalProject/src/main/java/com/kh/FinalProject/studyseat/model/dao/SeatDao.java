07a403739afa6498075c4e8e62f7a4771c35170f
user2 <user2@KH_G>
2019-11-06 21:45:13 +0900

    2019.11.06 데이터베이스와 연동해서 뷰 중간완성

--- FinalProject/src/main/java/com/kh/FinalProject/studyseat/model/dao/SeatDao.java
+++ FinalProject/src/main/java/com/kh/FinalProject/studyseat/model/dao/SeatDao.java
@@ -1,8 +1,24 @@
 package com.kh.FinalProject.studyseat.model.dao;
 
+import java.util.ArrayList;
+
+import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Repository;
+
+import com.kh.FinalProject.studyseat.model.vo.Seat;
 
 @Repository("sDao")
 public class SeatDao {
+
+	public ArrayList<Integer> countSeat(SqlSessionTemplate sqlSession) {
+		
+		return (ArrayList)sqlSession.selectList("seatMapper.countSeat");
+	}
+
+	
+	public ArrayList<Seat> selectSeatList(SqlSessionTemplate sqlSession) {
+	
+		return (ArrayList)sqlSession.selectList("seatMapper.selectSeatList");
+	}
 
 }
--- FinalProject/src/main/java/com/kh/FinalProject/studyseat/model/service/SeatService.java
+++ FinalProject/src/main/java/com/kh/FinalProject/studyseat/model/service/SeatService.java
@@ -1,8 +1,16 @@
 package com.kh.FinalProject.studyseat.model.service;
 
+import java.util.ArrayList;
+
 import org.springframework.stereotype.Service;
+
+import com.kh.FinalProject.studyseat.model.vo.Seat;
 
 @Service("sService")
 public interface SeatService {
+
+	ArrayList<Integer> countSeat();
+
+	ArrayList<Seat> selectSeatList();
 
 }

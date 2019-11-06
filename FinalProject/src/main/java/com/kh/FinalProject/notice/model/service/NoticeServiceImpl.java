42b9dbd32f0456c5143e18cc3569f3668408c8eb
heera <heera@Henry>
2019-11-06 21:33:35 +0900

    update

---
+++ FinalProject/src/main/java/com/kh/FinalProject/notice/model/service/NoticeServiceImpl.java
@@ -0,0 +1,44 @@
+package com.kh.FinalProject.notice.model.service;
+
+import java.util.ArrayList;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.kh.FinalProject.notice.model.dao.NoticeDAO;
+import com.kh.FinalProject.notice.model.vo.Notice;
+
+@Service("nService")
+public class NoticeServiceImpl {
+
+	@Autowired
+	private NoticeDAO nDAO;
+
+	@Autowired
+	private SqlSessionTemplate sqlSession;
+
+	// 공지사항 리스트
+	public ArrayList<Notice> selectList() {
+		return nDAO.selectList(sqlSession);
+	}
+
+	// 공지사항 디테일보기
+	public Notice selectNotice(int nId) {
+		
+		Notice n = null;
+		
+		int result = nDAO.addReadCount(sqlSession, nId);
+
+		if (result > 0) {
+			n = nDAO.selectNotice(sqlSession, nId);
+		}
+
+		return n;
+	}
+
+	public int inserNotice(Notice n) {
+		return nDAO.insertBoard(sqlSession, n);
+	}
+
+}

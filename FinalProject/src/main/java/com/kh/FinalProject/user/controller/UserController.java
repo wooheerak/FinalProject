a64f0437fbaaa8ce337789d47521cf0c0fd9bf4e
heera <heera@Henry>
2019-11-22 21:44:47 +0900

    우민파트 2차 업데이트

--- FinalProject/src/main/java/com/kh/FinalProject/user/controller/UserController.java
+++ FinalProject/src/main/java/com/kh/FinalProject/user/controller/UserController.java
@@ -3,6 +3,8 @@ package com.kh.FinalProject.user.controller;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.servlet.http.HttpSession;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,7 +12,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
 import org.springframework.web.servlet.ModelAndView;
@@ -226,11 +230,6 @@ public class UserController {
             mv.setViewName("userpasswordfind");
             return mv;
          }
-            
-         
-         
-   
-   
 
       }
 }
